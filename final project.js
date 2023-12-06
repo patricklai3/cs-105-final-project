faf09f13ed3c7ad0da151ca21d6e11df94cac3aa
patricklai3 <real2095@icloud.com>
2023-12-05 21:06:00 -0500

    incremental changes

--- final project.js
+++ final project.js
@@ -39,14 +39,15 @@ It's funny,
 */
 // all code goes below here ....
 
-const speed = 3;
+let speed = 3;
+let gameState = 0;
 let trackNumber = [1, 0];
-let victimX, victimY;
+let victimX, victimY, victimImg;
 let rockX, rockY;
 let wrenchX, wrenchY;
-let score = 0;
-let life = 3;
-let victim1Img, victim2Img, victim3Img, trolleyImg, guy1Img, guy2Img, guy3Img, driftImg, bannerImg;
+let score;
+let life;
+let victim1Img, victim2Img, victim3Img, trolleyImg, guy1Img, guy2Img, guy3Img, driftImg, bannerImg, rockImg, wrenchImg;
 
 function preload() {
     victim1Img = loadImage("victim1.png");
@@ -56,29 +57,41 @@ function preload() {
     guy1Img = loadImage("guy1.png");
     guy2Img = loadImage("guy2.png");
     guy3Img = loadImage("guy3.png");
-    driftImg = loadImage("drift.png");
+    driftImg = loadImage("drifting.png");
     bannerImg = loadImage("banner.png");
+    rockImg = loadImage("rock.png");
+    wrenchImg = loadImage("wrench.png");
 }
 
 function setup() {
-    createCanvas(800, 300);
+    createCanvas(800, 500);
     victimX = [800];
-    victimY = [100];
+    victimY = [360];
+    victimImg = [1];
     rockX = [800];
-    rockY = [200];
+    rockY = [460];
     wrenchX = [1000];
-    wrenchY = [100];
+    wrenchY = [360 + 200 / 800 * 260];
+    life = 1;
+    score = 0;
 }
 
 function draw() {
     background(255);
-
-    if (life == 100) {
+    if (gameState == 0) { // start screen
+        textAlign(CENTER);
+        background(255);
+        image(guy1Img, 300, 300, 700, 700);
         textSize(40);
-        text("Game Over", 300, 150);
-    } else {
-        drawTrack(100, 100);
-        drawTrack(200, 200);
+        text("The Trolley Problem \n but The Intrusive Thoughts Took Over", 400, 150);
+        textSize(20);
+        text("Press Any Key to Start", 400, 400);
+    } else if (gameState == 1) {
+        if (trackNumber[1] == 1) { // drift 
+            image(bannerImg, 0, 300, 800, 200);
+        }
+        drawTrack(100, 360);
+        drawTrack(200, 460);
 
         gameFlow();
         detectVictim();
@@ -86,120 +99,119 @@ function draw() {
         detectWrench();
         printScore();
 
-        train(); 
+        drawTrolley(); 
+    }
+    if (life == 0) { // game over
+        textSize(40);
+        textAlign(CENTER);
+        text("Game Over", 400, 250);
+        image(guy1Img, 300, 300, 700, 700);
+        textSize(20);
+        text("Press Any Key to Replay", 400, 400);
+        gameState = 3;
     }
 }
 
-function train() {
+
+function drawTrolley() {
     if (trackNumber[0] == 1 && trackNumber[1] == 0) {
-        rect(30, 70, 150, 60);
+        image(trolleyImg, 0, 15, 250, 250 / 3 * 2);
+        image(guy1Img, 600, 100, 100, 100);
     }
     if (trackNumber[0] == 2 && trackNumber[1] == 0) {
-        rect(30, 170, 150, 60);
+        image(trolleyImg, 0, 115, 250, 250 / 3 * 2);
+        image(guy2Img, 600, 100, 100, 100);
     }
     if (trackNumber[1] == 1) {
-        rect(30, 75, 60, 150);
+        image(driftImg, 0, 50, 300, 280);
+        image(guy3Img, 600, 100, 100, 100);
     }
 }
 
 function gameFlow() { 
     let x = random(0, 100); // generate victims
-    if (x < 1) { // 1% chance & not too close to the next rock
-        if (victimX[victimX.length - 1] > 800 - 50) {
-            victimX[victimX.length] = victimX[victimX.length - 1] + 50;
-            victimY[victimY.length] = Math.random() < 0.5 ? 100 : 200;
-        } else if (rockX[rockX.length - 1] > 800 - 100) {
-            victimX[victimX.length] = rockX[rockX.length - 1] + 50;
-            victimY[victimY.length] = Math.random() < 0.5 ? 100 : 200;
-        } else {
+    if (x < 1) {
             victimX[victimX.length] = 800;
-            victimY[victimY.length] = Math.random() < 0.5 ? 100 : 200;
-        }
+            victimY[victimY.length] = Math.random() < 0.5 ? 360 : 460;
+            victimImg[victimImg.length] = random(0, 100);
     }
     for (let i = 0; i < victimX.length; i++) { // move all victims
         victimX[i] -= speed;
-        drawVictim(victimX[i], victimY[i]);
+        victimY[i] -= speed / 800 * 260;
+        drawVictim(victimX[i], victimY[i], victimImg[i]);
     }
 
-    let y = random(0, 200); // generate rocks
-    if (y < 1) { // 0.2% chance & not too close to the next rock
-        if (victimX[victimX.length - 1] > 800 - 100) {
-            rockX[rockX.length] = victimX[victimX.length - 1] + 100;
-            rockY[rockY.length] = Math.random() < 0.5 ? 100 : 200;
-        } else if (rockX[rockX.length - 1] > 800 - 200) {
-            rockX[rockX.length] = rockX[rockX.length - 1] + 200;
-            rockY[rockY.length] = Math.random() < 0.5 ? 100 : 200;
-        } else {
-            rockX[rockX.length] = 800;
-            rockY[rockY.length] = Math.random() < 0.5 ? 100 : 200;
-        }
+    let y = random(0, 100); // generate rocks
+    if (y < 1) {
+        rockX[rockX.length] = 800;
+        rockY[rockY.length] = Math.random() < 0.5 ? 360 : 460;
     }
     for (let i = 0; i < rockX.length; i++) { // move all rocks
         rockX[i] -= speed;
+        rockY[i] -= speed / 800 * 260;
         drawRock(rockX[i], rockY[i]);
     }
 
-    let z = random(0, 2000); // generate wrenches
-    if (z < 1) { // 0.1% chance & not too close to the next rock
-       if (victimX[victimX.length - 1] > 800 - 50) {
-            wrenchX[wrenchX.length] = wrenchX[wrenchX.length - 1] + 2000;
-            wrenchY[wrenchY.length] = Math.random() < 0.5 ? 100 : 200;
-        } else if (rockX[rockX.length - 1] > 800 - 100) {
-            wrenchX[wrenchX.length] = rockX[rockX.length - 1] + 2000;
-            wrenchY[wrenchY.length] = Math.random() < 0.5 ? 100 : 200;
-        } else {
+    let z = random(0, 500); // generate wrenches
+    if (z < 1) {
             wrenchX[wrenchX.length] = 800;
-            wrenchY[wrenchY.length] = Math.random() < 0.5 ? 100 : 200;
-        }
+            wrenchY[wrenchY.length] = Math.random() < 0.5 ? 360 : 460;
     }
     for (let i = 0; i < wrenchX.length; i++) { // move all wrenches
         wrenchX[i] -= speed;
+        wrenchY[i] -= speed / 800 * 260;
         drawWrench(wrenchX[i], wrenchY[i]);
     }
-
 }
 
 function detectVictim() {
     for (let i = 0; i < victimX.length; i++) { // normal
-        if (victimX[i] > 30 && victimX[i] < 150) {
-            if (victimY[i] == 100 && trackNumber[0] == 1 && trackNumber[1] == 0) {
+        if (victimX[i] > 40 && victimX[i] < 180) {
+            if (victimY[i] < 190 && trackNumber[0] == 1 && trackNumber[1] == 0) {
                 victimX.splice(i, 1);
                 victimY.splice(i, 1);
+                victimImg.splice(i, 1);
                 score++;
-            } else if (victimY[i] == 200 && trackNumber[0] == 2 && trackNumber[1] == 0) {
+                speed++;
+            } else if (victimY[i] > 200 && victimY[i] < 300 && trackNumber[0] == 2 && trackNumber[1] == 0) {
                 victimX.splice(i, 1);
                 victimY.splice(i, 1);
+                victimImg.splice(i, 1);
                 score++;
+                speed++;
             }
         }
-        if (victimX[i] > 30 && victimX[i] < 90 && trackNumber[1] == 1) { // drift
+        if (victimX[i] > 40 && victimX[i] < 200 && trackNumber[1] == 1) { // drift
             victimX.splice(i, 1);
             victimY.splice(i, 1);
+            victimImg.splice(i, 1);
             score++;
+            speed++;
         }
         if (victimX[i] < 0) {
             victimX.splice(i, 1);
             victimY.splice(i, 1);
+            victimImg.splice(i, 1);
         }
     }
 }
 
 function detectRock() {
     for (let i = 0; i < rockX.length; i++) { // normal
-        if (rockX[i] > 30 && rockX[i] < 150) {
-            if (rockY[i] == 100 && trackNumber[0] == 1 && trackNumber[1] == 0) {
+        if (rockX[i] > 40 && rockX[i] < 180) {
+            if (rockY[i] < 180 && trackNumber[0] == 1 && trackNumber[1] == 0) {
                 rockX.splice(i, 1);
                 rockY.splice(i, 1);
                 score--;
                 life--;
-            } else if (rockY[i] == 200 && trackNumber[0] == 2 && trackNumber[1] == 0) {
+            } else if (rockY[i] > 200 && trackNumber[0] == 2 && trackNumber[1] == 0) {
                 rockX.splice(i, 1);
                 rockY.splice(i, 1);
                 score--;
                 life--;
             }
         }
-        if (rockX[i] > 30 && rockX[i] < 90 && trackNumber[1] == 1) { // drift
+        if (rockX[i] > 40 && rockX[i] < 200 && trackNumber[1] == 1) { // drift
             rockX.splice(i, 1);
             rockY.splice(i, 1);
             score--;
@@ -214,23 +226,26 @@ function detectRock() {
 
 function detectWrench() {
     for (let i = 0; i < wrenchX.length; i++) { // normal
-        if (wrenchX[i] > 30 && wrenchX[i] < 150) {
-            if (wrenchY[i] == 100 && trackNumber[0] == 1 && trackNumber[1] == 0) {
+        if (wrenchX[i] > 40 && wrenchX[i] < 180) {
+            if (wrenchY[i] < 200 && trackNumber[0] == 1 && trackNumber[1] == 0) {
                 wrenchX.splice(i, 1);
                 wrenchY.splice(i, 1);
                 score++;
+                speed++;
                 life++;
-            } else if (wrenchY[i] == 200 && trackNumber[0] == 2 && trackNumber[1] == 0) {
+            } else if (wrenchY[i] > 200 && wrenchY < 300 && trackNumber[0] == 2 && trackNumber[1] == 0) {
                 wrenchX.splice(i, 1);
                 wrenchY.splice(i, 1);
                 score++;
+                speed++;
                 life++;
             }
         }
-        if (wrenchX[i] > 30 && wrenchX[i] < 90 && trackNumber[1] == 1) { // drift
+        if (wrenchX[i] > 40 && wrenchX[i] < 200 && trackNumber[1] == 1) { // drift
             wrenchX.splice(i, 1);
             wrenchY.splice(i, 1);
             score++;
+            speed++;
             life++;
         }
         if (wrenchX[i] < 0) {
@@ -242,6 +257,7 @@ function detectWrench() {
 
 function printScore() {
     textSize(20);
+    textAlign(LEFT);
     text("Score: " + score, 10, 20);
     text("Life: " + life, 10, 40);
 }
@@ -251,18 +267,22 @@ function drawTrack(y1, y2) {
     line(0, y1 - 20, 800, y2 - 20);
 }
 
-function drawVictim(x, y) {
-    ellipse(x, y, 20, 20);
-    rect(x - 10, y + 10, 20, 30);
+function drawVictim(x, y, p) {
+    if (p < 33) {
+        image(victim1Img, x, y, 70, 70);
+    } else if (p < 66 & p > 33) {
+        image(victim2Img, x, y, 70, 70);
+    } else {
+        image(victim3Img, x, y, 70, 70);
+    }
 }
 
 function drawRock(x, y) {
-    circle(x, y, 50);
+    image(rockImg, x, y - 5, 50, 50);
 }
 
 function drawWrench(x, y) {
-    rect(x, y, 50, 10);
-    rect(x + 40, y - 5, 10, 20);
+    image(wrenchImg, x, y, 50, 50);
 }
 
 function mousePressed() {
@@ -276,4 +296,17 @@ function mouseReleased() {
         trackNumber[0] = 1;
     }
     trackNumber[1] = 0; // reset
+}
+
+function keyPressed() {
+    gameState = 1;
+    victimX = [800];
+    victimY = [360];
+    victimImg = [1];
+    rockX = [800];
+    rockY = [460];
+    wrenchX = [1000];
+    wrenchY = [360 + 200 / 800 * 260];
+    life = 1;
+    score = 0;
 }
